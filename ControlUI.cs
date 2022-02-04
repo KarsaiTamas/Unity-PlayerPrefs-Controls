0c04ae145f6654b33aa43bc5b6712cd4862b0475
KarsaiTamas <43405260+KarsaiTamas@users.noreply.github.com>
2022-02-04 20:58:46 +0100

    Update ControlUI.cs

--- ControlUI.cs
+++ ControlUI.cs
@@ -29,43 +29,38 @@ public class ControlUI : MonoBehaviour
     {
         if (!isActive) return;
 
-        if (Input.anyKey)
+        if (!Input.anyKey) return;
+        
+        Event e = Event.current;
+        KeyCode key = e.keyCode;
+        //This is for all mouse buttons
+        //for some reason Event can't get every mouse button
+        //also it doesn't even know when I click with the mouse
+        //it recognizes it as an event but not as a mouse event
+        for (int i = (int)KeyCode.Mouse0; i <= (int)KeyCode.Mouse6; i++)
         {
-            Event e = Event.current;
-            KeyCode key = e.keyCode;
-            //This is for all mouse buttons
-            //for some reason Event can't get every mouse button
-            //also it doesn't even know when I click with the mouse
-            //it recognizes it as an event but not as a mouse event
-            for (int i = (int)KeyCode.Mouse0; i <= (int)KeyCode.Mouse6; i++)
+            if (Input.GetKey((KeyCode)i))
             {
-                if (Input.GetKey((KeyCode)i))
-                {
-                    Debug.Log(i);
-                    key = (KeyCode)i;
-                    break;
-                }
+                Debug.Log(i);
+                key = (KeyCode)i;
+                break;
             }
-
-            if (Controls.keys[control] == key)
-            {
-                isActive = false;
-                button.interactable = !isActive;
-                enabled = isActive;
-                return;
-            }
-            
-            PlayerPrefs.SetInt(control.ToString(), (int)key);
-            Controls.keys[control] = key;
-            Controls.FlagDublicates();
-            KeyBoundtext.text = key.ToString().Equals("Return")? "Enter":key.ToString();
-            isActive = false;
-            enabled = isActive;
-            button.interactable = !isActive;
-
-
-
         }
 
+        if (Controls.keys[control] == key)
+        {
+            isActive = false;
+            button.interactable = !isActive;
+            enabled = isActive;
+            return;
+        }
+
+        PlayerPrefs.SetInt(control.ToString(), (int)key);
+        Controls.keys[control] = key;
+        Controls.FlagDublicates();
+        KeyBoundtext.text = key.ToString().Equals("Return")? "Enter":key.ToString();
+        isActive = false;
+        enabled = isActive;
+        button.interactable = !isActive;
     }
 }
